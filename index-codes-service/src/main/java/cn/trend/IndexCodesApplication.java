49b6a04bf3ba78aae7b0adcbb694e922fa33c61f
Xuanzhihao <xuanzhihao1997@foxmail.com>
2020-03-04 16:31:29 +0800

    add zipkin-server , IndexConfigServer with rabbitMQ

--- index-codes-service/src/main/java/cn/trend/IndexCodesApplication.java
+++ index-codes-service/src/main/java/cn/trend/IndexCodesApplication.java
@@ -1,5 +1,6 @@
 package cn.trend;
 
+import brave.sampler.Sampler;
 import cn.hutool.core.convert.Convert;
 import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.core.util.NetUtil;
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
@@ -85,5 +87,10 @@ public class IndexCodesApplication
             System.exit(1);
         }
         new SpringApplicationBuilder(IndexCodesApplication.class).properties("server.port=" + port).run(args);
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
     }
 }
--- trend-trading-backtest-view/src/main/java/cn/trend/web/ViewController.java
+++ trend-trading-backtest-view/src/main/java/cn/trend/web/ViewController.java
@@ -1,12 +1,20 @@
 package cn.trend.web;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RefreshScope
 public class ViewController {
+    @Value("${version}")
+    String version;
+
     @GetMapping("/")
-    public String view() throws Exception{
+    public String view(Model model) throws Exception{
+        model.addAttribute("version", version);
         return "view";
     }
 }
