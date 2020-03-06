2deb078cea0eb269671c95cb4fe4b1b6c04e5ced
Xuanzhihao <xuanzhihao1997@foxmail.com>
2020-03-06 09:41:17 +0800

    add index-turbine and index-hystrix-dashboard

--- trend-trading-backtest-service/src/main/java/cn/trend/TrendTradingBackTestServiceApplication.java
+++ trend-trading-backtest-service/src/main/java/cn/trend/TrendTradingBackTestServiceApplication.java
@@ -8,6 +8,7 @@ import cn.hutool.core.util.NumberUtil;
 import cn.hutool.core.util.StrUtil;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -27,6 +28,7 @@ import java.util.concurrent.TimeoutException;
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class TrendTradingBackTestServiceApplication {
 
     public static void main(String[] args) {
